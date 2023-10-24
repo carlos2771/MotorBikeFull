b388c4f2a0416f7336036b5a59cacc5bc2b937c1
Camilo Estrada Patiño <caespa0818@hotmail.es>
2023-10-23 23:29:16 -0500

    Arreglo mi axiosInstance

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,11 +3,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const axiosClient = axios.create({
-<<<<<<< Updated upstream
-  baseURL: "http://localhost:3000/api",
-=======
-  baseURL: "http://localhost:3000/api/",
->>>>>>> Stashed changes
+  baseURL: BASE_URL,
   withCredentials: true, // para poder leer las cookies
   
 })
