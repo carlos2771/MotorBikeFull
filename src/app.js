8d7e8974c7165eafc6ab4d22a2b434ab43abbc08
SantiagoPulgarin415 <lgsantiago516@gmail.com>
2023-10-23 22:29:06 -0500

    Ventas_Servicios

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import taskRoutes from "./routes/tasks.routes.js"
 import clientesRoutes from "./routes/clientes.routes.js"
 import mecanicosRoutes from "./routes/mecanicos.routes.js"
+import ventas_serviciosRoutes from "./routes/ventas_servicios.routes.js"
 import marcaRoutes from "./routes/marca.routes.js"
 import cors  from 'cors'
 
@@ -30,5 +31,6 @@ app.use("/api", taskRoutes)
 app.use("/api", clientesRoutes)
 app.use("/api", mecanicosRoutes )
 app.use("/api", marcaRoutes )
+app.use("/api", ventas_serviciosRoutes)
 
 export default app;
---
+++ src/controllers/ventas_servicios.controller.js
@@ -0,0 +1,56 @@
+import  Ventas_Servicios from "../models/ventas_servicios.model.js"
+import  Cliente from "../models/cliente.model.js"
+export const getVentas_Servicios = async(req, res) =>{
+    const ventas_servicios = await Ventas_Servicios.find()
+    if(!ventas_servicios) return res.status(404).json({message: "Venta_Servicios no encontrados"})
+    res.json(ventas_servicios)
+
+}
+
+export const createVentas_Servicios = async (req, res) => {
+    try {
+        const { cliente: clienteId, precio_servicio, descripcion } = req.body;
+
+        // Verifica si el cliente existe
+        const clienteEncontrado = await Cliente.findById(clienteId);
+        if (!clienteEncontrado) {
+            return res.status(404).json({ message: 'Cliente no encontrado' });
+        }
+
+        // Crea una nueva venta de servicio asociada al cliente
+        const nuevaVentaServicio = new Ventas_Servicios({
+            cliente: clienteEncontrado,
+            precio_servicio,
+            descripcion
+        });
+
+        // Guarda la venta de servicio en la base de datos
+        const ventaServicioGuardada = await nuevaVentaServicio.save();
+
+        res.status(201).json(ventaServicioGuardada);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear la venta de servicio', error });
+    }
+};
+
+  
+  
+  
+  
+  
+  
+  
+
+export const deleteVentas_Servicios = async(req, res) =>{
+    const ventas_servicios = await Ventas_Servicios.findByIdAndDelete(req.params.id)
+    if(!ventas_servicios) return res.status(404).json({message: "ventas_servicios not found"})
+    return res.sendStatus(204)
+}
+
+export const updateVentas_Servicios= async(req, res) =>{
+    const ventas_servicios = await Ventas_Servicios.findByIdAndUpdate(req.params.id, req.body,{ // new y true son para que el guarde los datos nuevos que ingrese el usuario
+        new: true
+    })
+    if(!ventas_servicios) return res.status(404).json({message: "ventas_servicios not found"})
+    res.json(ventas_servicios)
+}
