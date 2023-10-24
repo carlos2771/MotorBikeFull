e9161e3e3389dbba2425559c07d6d027ad915927
Camilo Estrada Patiño <caespa0818@hotmail.es>
2023-10-23 21:28:02 -0500

    Actualizacion rama Camilo

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,7 +3,11 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const axiosClient = axios.create({
+<<<<<<< Updated upstream
   baseURL: "http://localhost:3000/api",
+=======
+  baseURL: "http://localhost:3000/api/",
+>>>>>>> Stashed changes
   withCredentials: true, // para poder leer las cookies
   
 })
