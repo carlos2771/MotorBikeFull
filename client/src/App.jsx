85c64ae77985dadd7aaccab0bc567324fd931677
carlos2771 <carlosgamerki412@gmail.com>
2023-10-25 21:59:28 -0500

    "actualizacion de main"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegisterPage from "./pages/registerPage";
+import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./context/AuthContext";
 import TaskPaje from "./pages/TaskPaje";
