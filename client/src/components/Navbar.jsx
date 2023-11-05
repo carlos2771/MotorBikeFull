eea76f80e28e82ded78c673227252a7d40b05e37
saritalop <saritalop789@gmail.com>
2023-11-05 15:35:56 -0500

    traje los cambios de develop

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,72 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import Render from "./Render";
+import Header from "./Header";
 
 export default function Navbar() {
   const { isAuthenticated, logout, user } = useAuth();
+  const [open, setOpen] = useState(false);
   console.log(user);
   
   return (
-    <nav className="bg-zinc-700 my-2 flex justify-between py-5 px-10 rounded-lg ">
-      <Link to={
-        isAuthenticated ? "/tasks" : "/"
-      }>
-        <h1 className="text-2xl font-bold">Motor Bike</h1>
-      </Link>
-      
-      <ul className="flex gap-x-2">
-        {isAuthenticated ? (
-          <>
-            
-           <li>
-                Bienvenido {user.username}
-            </li>
-            <li>
-                <Link to="/tasks">Tareas</Link>
-            </li>
-            <li>
-                <Link to="/add-task">agg Tareas</Link>
-            </li>
-            <li>
-                <Link to="/clientes">Clientes</Link>
-            </li>
-            <li>
-                <Link to="/mecanicos">Mecanicos</Link>
-            </li>
-            <li>
-                <Link to="/marcas">Marcas</Link>
-            </li>
-            <li>
-                <Link to="/ventas-respuestos" className="">Ventas Repuestos</Link>
-            </li>
-            <li>
-                <Link to="/permisos" className="">Permisos</Link>
-            </li>
-            <li >
-                <Link className="px-5 py-1 text-sm text-withe font-semibold rounded-full border border-indigo-500 hover:text-white hover:bg-indigo-500 hover:border-transparent shadow-lg shadow-zinc-300/30 "
+    <div>
+      {isAuthenticated ? (
+        <>
+          <div className="bg-zinc-700 py-3 fixed top-0 left-0 right-0 shadow-md" style={{ zIndex: 1000 }}>
+            <button className="ml-4" onClick={() => setOpen(true)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+
+            <div
+              className={`${
+                !open && "hidden"
+              } bg-gray-600/50 min-h-screen  fixed top-0 left-0 right-0 transition-all duration-300`}
+              onClick={() => setOpen(false)}
+            ></div>
+
+            <div
+              className={`${
+                open ? "w-80" : "w-0"
+              } bg-cyan-600 min-h-screen fixed top-0 left-0 transition-all duration-300`}
+            >
+              <div className={`${!open && "hidden"} pt-3`}>
+                <button
+                  className="ml-4 text-white mb-14"
+                  onClick={() => setOpen(false)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+                <Render>
+                  <Link to="/tasks">Tareas</Link>
+                </Render>
+                <Render>
+                  <Link to="/add-task">agg Tareas</Link>
+                </Render>
+                <Render>
+                  <Link to="/clientes">clientes</Link>
+                </Render>
+                <Render>
+                  <Link to="/ventas-respuestos" className="">
+                    Ventas Repuestos
+                  </Link>
+                </Render>
+                <Render>
+                  <Link to="/permisos" className="">
+                    Permisos
+                  </Link>
+                </Render>
+                <Render>
+                <Link 
                 to="/" onClick={()=>{
                     logout()
                 }}>Logout</Link>
-            </li>
-            
-            
-          </>
-        ) : (
-          <>
-            <li>
-              <Link className="px-5 py-1 text-sm text-withe font-semibold rounded-full border border-blue-500 hover:text-white hover:bg-blue-500 hover:border-transparent shadow-lg shadow-zinc-300/30" 
-              to="/login">Login</Link>
-            </li>
-            <li>
-              <Link className="px-5 py-1 text-sm text-withe font-semibold rounded-full border border-blue-500 hover:text-white hover:bg-blue-500 hover:border-transparent shadow-lg shadow-zinc-300/30"
-               to="/register">Register</Link>
-            </li>
-          </>
-        )}
-      </ul>
-      
-    </nav>
-
-    
+                </Render>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <Header />
+        </>
+      )}
+    </div>
   );
 }
