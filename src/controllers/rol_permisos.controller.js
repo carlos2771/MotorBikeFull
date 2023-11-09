48ff8bd4d851898f747ee1fea595bc566dc3f84a
Camilo Estrada Patiño <caespa0818@hotmail.es>
2023-11-08 22:58:55 -0500

    Controller de rol_permiso

--- src/controllers/rol_permisos.controller.js
+++ src/controllers/rol_permisos.controller.js
@@ -1,101 +1,88 @@
-import Rol_Permiso from "../models/rol_permiso.model.js"
-import Repuesto from "../models/repuestos.model.js";
-import Cliente from "../models/cliente.model.js";
+import Rol_Permisos from "../models/rol_permiso.model.js"
+import Rol from "../models/roles.model.js";
+import Permiso from "../models/cliente.model.js";
 
 // Todos los roles X Permiso
 export const getRol_Permisos = async (req, res) => {
   try {
-    const ventas_repuestos = await Ventas_Repuestos.find().populate({ path: 'roles', select: 'nombre_rol' }) .populate({ path: 'permisos', select: 'nombre_permiso' });
-    if (!ventas_repuestos) {
-      return res.status(404).json({ message: "Venta_Repuestos no encontrados" });
+    const rol_permisos = await Rol_Permisos.find().populate({ path: 'roles', select: 'nombre_rol' }) .populate({ path: 'permisos', select: 'nombre_permiso' });
+    if (!rol_permisos) {
+      return res.status(404).json({ message: "Rol permiso no encontrados" });
     }
-    res.json(ventas_repuestos);
+    res.json(rol_permisos);
   } catch (error) {
-    return res.status(500).json({ message: "Error al obtener ventas de repuestos", error });
+    return res.status(500).json({ message: "Error al obtener permisos de rol", error });
   }
 };
 
 // 1 Rol x permiso
 export const getRol_Permiso = async (req, res) => {
   try {
-    const venta_repuesto = await Ventas_Repuestos.findById(req.params.id)
-    if (!venta_repuesto) {
-      return res.status(404).json({ message: "venta_repuesto not found" });
+    const rol_permiso = await Rol_Permisos.findById(req.params.id)
+    if (!rol_permiso) {
+      return res.status(404).json({ message: "rol permiso no encontrado" });
     }
-    res.json(venta_repuesto);
+    res.json(rol_permiso);
   } catch (error) {
-    return res.status(500).json({ message: "Error al obtener la venta de repuesto", error });
+    return res.status(500).json({ message: "Error al obtener permisos de rol", error });
   }
 };
 
 export const createRol_Permiso = async (req, res) => {
   try {
     const {
-      repuesto: repuestoId,
-      cantidad_repuesto,
-      precio_unitario,
-      precio_total,
-      cliente: clienteId,
+      rol: rolId,
+      permiso: permisoId,
     } = req.body;
 
     // Verifica si el cliente existe
-    const clienteEncontrado = await Cliente.findById(clienteId);
-    if (!clienteEncontrado) {
-      return res.status(404).json({ message: "Cliente no encontrado" });
+    const permisoEncontrado = await Permiso.findById(permisoId);
+    if (!permisoEncontrado) {
+      return res.status(404).json({ message: "Permiso no encontrado" });
     }
-    const repuestoEncontrado = await Repuesto.findById(repuestoId);
-    if (!repuestoEncontrado) {
-      return res.status(404).json({ message: "Repuesto no encontrado" });
+    const rolEncontrado = await Rol.findById(rolId);
+    if (!rolEncontrado) {
+      return res.status(404).json({ message: "Rol no encontrado" });
     }
-    // Obtiene la cantidad actual del repuesto
-    const cantidadActualRepuesto = repuestoEncontrado.cantidad;
+
+
+    // // Obtiene la cantidad actual del repuesto
+    // const cantidadActualRepuesto = permisoEncontrado.permiso;
   
-    // Verifica si hay suficiente cantidad disponible del repuesto
-    if (cantidadActualRepuesto < cantidad_repuesto) {
-      return res.status(400).json({ message: ["Cantidad insuficiente del repuesto"] });
-    }
-
-    // Resta la cantidad vendida de la cantidad actual del repuesto
-    const cantidadRestanteRepuesto = cantidadActualRepuesto - cantidad_repuesto;
-    await Repuesto.findByIdAndUpdate(repuestoId, { cantidad: cantidadRestanteRepuesto });
-
-    // Crea una nueva venta de servicio asociada al cliente
+    // Crea un nuevo permiso asociada rol
     const nuevoRolPermiso = new Rol_Permiso({
-      repuesto: repuestoId,
-      cantidad_repuesto,
-      precio_unitario,
-      precio_total,
-      cliente: clienteId,
+      rol: rolId,
+      permiso: permisoId,
     });
 
-    // Guarda la venta de servicio en la base de datos
+    // Guarda el rol permiso en la base de datos
     const rolPermisoGuardada = await nuevoRolPermiso.save();
 
-    res.status(201).json(ventaRepuestoGuardada);
+    res.status(201).json(rolPermisoGuardada);
   } catch (error) {
-    return res.status(500).json({ message: "Error al crear la venta de repuesto", error });
+    return res.status(500).json({ message: "Error al crear el permiso del rol", error });
   }
 };
 
 
 export const updateRol_Permisos= async(req, res) =>{
   try {
-    const venta_repuesto = await Ventas_Repuestos.findByIdAndUpdate(req.params.id, req.body, {
+    const rol_permiso = await Rol_Permisos.findByIdAndUpdate(req.params.id, req.body, {
       // new y true son para que el guarde los datos nuevos que ingrese el usuario
       new: true,
     });
-    if (!venta_repuesto) return res.status(404).json({ message: "venta repuesto not found" });
-    res.json(venta_repuesto);
+    if (!rol_permiso) return res.status(404).json({ message: "Rol permiso no encontrada" });
+    res.json(rol_permiso);
   } catch (error) {
-    return res.status(500).json({ message: "venta repuesto no encontrada" });
+    return res.status(500).json({ message: "Rol permiso no encontrada" });
   }
 }
 
 export const deleteRol_Permisos = async(req, res) =>{
   try {
-    const deletedVenta_repuesto = await Ventas_Repuestos.findByIdAndDelete(req.params.id);
-    if (!deletedVenta_repuesto)
-      return res.status(404).json({ message: "Venta  not found" });
+    const deletedRol_permiso = await Rol_Permisos.findByIdAndDelete(req.params.id);
+    if (!deletedRol_permiso)
+      return res.status(404).json({ message: "Rol permiso no encontrado" });
     return res.sendStatus(204);
   } catch (error) {
     return res.status(500).json({ message: error.message });
