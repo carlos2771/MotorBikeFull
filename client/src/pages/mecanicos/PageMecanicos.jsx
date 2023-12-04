a5013b3efa40bb76854cab2e007ec4bc16c78a74
saritalop <saritalop789@gmail.com>
2023-12-04 14:19:12 -0500

    cambios mecanico

--- client/src/pages/mecanicos/PageMecanicos.jsx
+++ client/src/pages/mecanicos/PageMecanicos.jsx
@@ -141,7 +141,7 @@ export default function PageMecanico() {
     },
     {
         field: "cedula_mecanico",
-        headerName: "Cedula",
+        headerName: "Cédula",
         width: 200,
         headerClassName: "font-custom text-lg",
     },
@@ -200,12 +200,12 @@ export default function PageMecanico() {
         return (
           <div>
             <button className={estado === "Activo" ? "" : "hidden"}>
-            <Link to="/add-mecanico">
-              <button  className="px-4 py-2 m-2 text-sm text-withe font-semibold rounded-full border border-sky-500 hover:text-white hover:bg-sky-500 hover:border-transparent">
-              <FontAwesomeIcon icon={faPlus} className="mr-2" />
-                Agregar
-              </button>
-            </Link>
+              <Link
+                className="px-4 py-1.5 m-1 text-sm text-white font-semibold rounded-full border border-indigo-500 hover:text-white hover:bg-indigo-500"
+                to={`/mecanico/${params.row._id}`}
+              >
+                <FontAwesomeIcon icon={faPencil} />
+              </Link>
             </button>
           {/* <button
             className="px-4 py-1 m-1 text-sm text-white font-semibold rounded-full border border-red-500 hover:text-white hover-bg-red-500"
@@ -323,7 +323,7 @@ export default function PageMecanico() {
 
   return (
     <div className="mt-16">
-      <h1 className="text-2xl mx-auto ml-20 font-custom"> <FontAwesomeIcon icon="wrench" className="mr-2" />Gestion de Mecánicos</h1>
+      <h1 className="text-2xl mx-auto ml-20 font-custom"> <FontAwesomeIcon icon="wrench" className="mr-2" />Gestión de Mecánicos</h1>
       <div className="mx-20 ml-2 justify-end flex">
         <Link to="/add-mecanico">
           <button  className="px-4 py-2 m-2 text-sm text-withe font-semibold rounded-full border border-sky-500 hover:text-white hover:bg-sky-500 hover:border-transparent">
