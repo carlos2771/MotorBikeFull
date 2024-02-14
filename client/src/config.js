0330b316f4867347e9d190005cb43c7500989cdd
Camilo <caespa0818@hotmail.es>
2024-02-14 16:56:04 -0500

    Cambios js

--- client/src/config.js
+++
@@ -1,2 +0,0 @@
-export const TOKEN_SECRET = "alguna llave secreta" 
-export const secretKey = process.env.JWT_SECRET || 'secreto';
--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-import { connectDB } from "./db.js"; // have keys for that exportation is without default
-
-connectDB() // inicialice la conexion
-
-app.listen(3000)
-console.log("server port", 3000);
