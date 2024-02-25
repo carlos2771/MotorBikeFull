55c89a7ed380af67d22872c99c101c49f644c46c
saritalop <saritalop789@gmail.com>
2024-02-25 18:43:31 -0500

    cambios en el menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { useAuth } from "../hooks/useAuth";
 import Render from "./Render";
 import Header from "./Header";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTasks, faUsers, faMotorcycle, faTools, faShoppingCart, faHandshake, faShoppingBag, faChartBar, faSignOutAlt, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTasks, faUsers, faMotorcycle, faTools, faShoppingCart, faHandshake, faShoppingBag, faChartBar, faSignOutAlt, faGears,faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import logo from '../pages/images/motorbike.png';
 
 export default function Navbar() {
   const { isAuthenticated, logout, user } = useAuth();
@@ -46,7 +47,7 @@ export default function Navbar() {
             ></div>
 
             <div
-              className={`${open ? "w-80" : "w-0"
+              className={`${open ? "w-64" : "w-0"
                 } bg-slate-700 min-h-screen fixed top-0 left-0 transition-all duration-300`}
             >
               <div className={`${!open && "hidden"} pt-3`}>
@@ -54,57 +55,60 @@ export default function Navbar() {
                   className="ml-4 text-white "
                   onClick={() => setOpen(false)}
                 >
-                 <FontAwesomeIcon icon={faChevronLeft} />
+                 {/* <FontAwesomeIcon icon={faChevronLeft} /> */}
+                 {/* <img src={logo} alt="Logo" className="w-26 h-26  items-center justify-center" /> */}
+                 <h1 className=" flex ms-5 p-2 text-[24px] font-bold text-white"><FontAwesomeIcon icon={faGears} className="mr-2"/>  Motor <span className='text-blue-300'>Bike</span></h1> 
                 </button>
                 <div onClick={() => setOpen(false)}>
+                <br />  
                 <Render>
                     <Link
                       to="/graficos"  
-                      className="flex ms-10 p-2"
+                      className="flex ms-8 p-3"
                     >
                       <FontAwesomeIcon icon={faChartBar} className="mr-2" /> Dashboard
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/rol" className="flex ms-10 p-2">
+                    <Link to="/rol" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faUsers} className="mr-2" /> Roles
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/usuarios" className="flex ms-10 p-2">
+                    <Link to="/usuarios" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faUsers} className="mr-2" /> Usuarios
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/clientes" className="flex ms-10 p-2">
+                    <Link to="/clientes" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faUsers} className="mr-2" /> Clientes
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/mecanicos" className="flex ms-10 p-2">
+                    <Link to="/mecanicos" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon="wrench" className="mr-2" /> Mecánicos
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/marcas" className="flex ms-10 p-2">
+                    <Link to="/marcas" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faMotorcycle} className="mr-2" /> Marcas
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/repuestos" className="flex ms-10 p-2">
+                    <Link to="/repuestos" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faTools} className="mr-2" /> Repuestos
                     </Link>
                   </Render>
                   <Render>
                     <Link
                       to="/compras"
-                      className="flex ms-10 p-2"
+                      className="flex ms-8 p-3"
                     >
                       <FontAwesomeIcon icon={faShoppingBag} className="mr-2" /> Compras
                     </Link>
                   </Render>
                   <Render>
-                    <Link to="/home-page" className="flex ms-10 p-2">
+                    <Link to="/home-page" className="flex ms-8 p-3">
                       <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />Ventas repuestos
                     </Link>
                   </Render>
@@ -119,7 +123,7 @@ export default function Navbar() {
                   <Render>
                     <Link
                       to="/ventas-servicios"
-                      className="flex ms-10 p-2"
+                      className="flex ms-8 p-3"
                     >
                       <FontAwesomeIcon icon={faHandshake} className="mr-2" /> Ventas Servicios
                     </Link>
@@ -127,7 +131,7 @@ export default function Navbar() {
                   <Render>
                     <Link
                       to="/tasks"
-                      className="flex ms-10 p-2"
+                      className="flex ms-8 p-3"
                     >
                       <FontAwesomeIcon icon={faTasks} className="mr-2" /> Tareas
                     </Link>
