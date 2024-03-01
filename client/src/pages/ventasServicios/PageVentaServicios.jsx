d95653299d94f9381030a2abafc476173f900c94
Camilo Estrada Patiño <automatizador8@ariafina.co>
2024-02-29 22:23:29 -0500

    Roles arreglado

--- client/src/pages/ventasServicios/PageVentaServicios.jsx
+++ client/src/pages/ventasServicios/PageVentaServicios.jsx
@@ -39,6 +39,7 @@ export default function PageVentaServicios() {
     deleteVentaServicio,
     updateVentaServicio,
   } = useVentasServicios();
+  
   const { user } = useAuth();
 
   useEffect(() => {
