3acb8eefddc3b4b13087520efec122e05b08884d
saritalop <saritalop789@gmail.com>
2024-03-05 17:27:19 -0500

    excel - venta repuesto

--- client/src/pages/cartClient/PageCartClient.jsx
+++ client/src/pages/cartClient/PageCartClient.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useCartCliente } from "../../context/CartClienteContext";
 import { useClientes } from "../../context/ClientContext";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
@@ -9,6 +9,7 @@ import { Tabla, Titulo } from "../../components/Tabla";
 import Detalle from "../../components/Detalle";
 import Swal from "sweetalert2";
 import MUIDataTable from "mui-datatables";
+import * as XLSX from 'xlsx';
 
 import { useAuth } from "../../hooks/useAuth";
 
@@ -17,6 +18,7 @@ import {
   faTools,
   faPlus,
   faBan,
+  faDownload,
   faInfoCircle,
   faDollarSign,
   faScrewdriverWrench,
@@ -27,6 +29,7 @@ import {
 export default function PageCartClient() {
   const { getCartClient, cartClientes, getCartCliente, updateCartCliente } =
     useCartCliente();
+    const [dataForExcel, setDataForExcel] = useState([]);
 
   const { user } = useAuth();
 
@@ -57,6 +60,65 @@ export default function PageCartClient() {
       console.error("Error al obtener todo:", error);
     }
   }, []);
+
+  useEffect(() => {
+
+    // Transforma los datos en el formato adecuado para el archivo Excel
+    const newDataForExcel = cartClientes.map((venta) => ({
+      Codigo: venta.codigo,
+      Cliente: venta.cliente.nombre_cliente,
+      Descuento: venta.descuento,
+      "Total Venta": venta.total,
+      Repuestos: venta.cart.map((repuesto) => ({
+        Nombre: repuesto.name,
+        Cantidad: repuesto.amount,
+        Marca: repuesto.marca,
+      })),
+    }));
+    setDataForExcel(newDataForExcel);
+  }, [cartClientes]);
+
+  const exportarAExcel = () => {
+    // Define una función para combinar los repuestos de cada venta en una cadena de texto
+    const obtenerRepuestos = (repuestos) => {
+      return repuestos.map((r) => `Repuesto: ${r.Nombre}\n Cantidad: ${r.Cantidad}`).join("\n");
+    };
+
+    // Calcula el total de todas las ventas
+    const totalVentas = dataForExcel.reduce((total, venta) => total + venta["Total Venta"], 0);
+
+    // Transforma los datos en un formato adecuado para el archivo Excel
+    const data = dataForExcel.map((venta) => ({
+      
+      
+      Codigo: venta.Codigo,
+      Cliente: venta.Cliente,
+      Descuento: venta.Descuento,
+      Repuestos: obtenerRepuestos(venta.Repuestos),
+      "Total Venta": venta["Total Venta"],
+      
+    }));
+
+    // Agrega una fila con el total de ventas al final del array de datos
+    data.push({
+      Codigo: "",
+      Cliente: "",
+      Descuento: "",
+      Repuestos: "TOTAL DE TODAS LAS VENTAS",
+      "Total Venta": totalVentas,
+     
+    });
+
+    // Crea una hoja de cálculo en formato de libro de trabajo de Excel
+    const ws = XLSX.utils.json_to_sheet(data);
+
+    // Crea un nuevo libro de trabajo de Excel y agrega la hoja de cálculo
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Ventas");
+
+    // Guarda el libro de trabajo de Excel como un archivo
+    XLSX.writeFile(wb, "ventas.xlsx");
+  };
 
   const mostrarAlerta = (id, anulado) => {
     const title = anulado ? "Anulado" : "Anular";
@@ -468,6 +530,13 @@ export default function PageCartClient() {
                   <FontAwesomeIcon icon={faPlus} />
                 </button>
               </Link>
+              <button
+                onClick={exportarAExcel}
+                className="px-4 py-2 mx-2 text-sm text-withe font-semibold rounded-full border border-green-600 hover:text-white hover:bg-green-600 hover:border-transparent"
+                title="Descargar excel"
+              >
+                <FontAwesomeIcon icon={faDownload} />
+              </button>
             </div>
           </div>
 
